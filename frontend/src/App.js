7f9653a6a8c47a83432d85dbbebd6a31aeeee9cb
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-09 14:52:17 +0530

    Add proper comment in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 /* 
- * HOMEPAGE Of Appl cation
+ * Base Template Of Application
  */
+
 import { Outlet }    from 'react-router-dom';
 
 // Local Files
 import Header        from './components/Header';
 import Footer        from './components/Footer';
 
+/*
+ * Router uses the outlet ( index.js )
+ * like which component need to show
+ * there
+ */
 const App = () => {
   return (
     <>
       <Header />
+
+      {/* From router path */}
       <Outlet />
+
       <Footer />
     </>
   );
